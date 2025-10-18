879ad8734bf88b1d42c13c695f477cc34fb0a388
sonali0227 <sona27092002@gmail.com>
2025-10-18 22:59:54 +0000

    DataType Summary

---
+++ datatypes-summary.js
@@ -0,0 +1,13 @@
+/* video no.-9
+data types divided into two category
+1 Primitive
+it is 7 types : String, Number, Boolean, null, Undefined, Symbol, BigInt
+
+2 Non Primitive or Reference Type (memory me direct refernce inka allocate kiya ja skta hai)
+Array, Functions, Objects(to master js then master its objects, browser web events or browser events).
+
+Q. js is dynamically type language or statically type language??
+
+
+*/
+
